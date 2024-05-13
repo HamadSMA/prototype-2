49a11f32670d9c19e7f1791928817515e0cf8cc8
Hamad Alaslani <Dasheightair@gmail.com>
2024-05-13 22:57:48 +0300

    health bar and function to track health

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -28,9 +28,10 @@ public class DetectCollisions : MonoBehaviour
         }
         else if (other.CompareTag("Animal"))
         {
-            gameManager.AddScore(5);
+            // gameManager.AddScore(5);
+            other.GetComponent<AnimalHunger>().FeedAnimal(1);
             Destroy(gameObject);
-            Destroy(other.gameObject);
+            // Destroy(other.gameObject);
         }
 
     }
